8991a5ed8d3299a75934efe54153eaacd6bab7ec
dontwait <nguyentansang15042005@gmail.com>
2025-07-31 20:59:39 +0700

    feat: add jinja2template for gen template

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request 
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/")
-async def root():
-    return {"message": "Welcome to the WebSocket server!"}
+async def root(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "index.html",
+        {}
+    )
